b4a69f1ed88cd52d57c6325be5a0ccd19bcea0d7
Nick Bansal <https://github.com/NickBansal>
2018-12-07 09:48:34 +0000

    Start game button added

--- src/BreakOut.js
+++ src/BreakOut.js
@@ -4,16 +4,14 @@ import './App.css';
 class App extends Component {
 
   state = {
-    paddleHeight: 20,
-    paddleWidth: 80,
     rightPressed: true, 
     leftPressed: false,
-    game: true,
+    game: false,
     paddleX: (650-80)/2
   }
 
   render() {
-    // this.updateCanvas()
+
     return (
       <div className="App">
         <h1>Breakout</h1>
@@ -23,71 +21,23 @@ class App extends Component {
         onKeyUp={(e) => this.keyUpHandler(e.key)}
         ref="canvas" 
         width={650} 
-        height={350}/>
-        <button>Start Game</button>
+        height={350} />
+        {!this.state.game && <button onClick={this.handleClick}>START</button>}
       </div>
     );
   }
 
-  drawPaddle = ctx => {
-    ctx.clearRect(0, 0, 200, 200);
-    const { rightPressed, leftPressed, paddleWidth, paddleHeight } = this.state
-    let paddleX = this.state.paddleX
-    let newPaddle
-    ctx.beginPath();
-    ctx.rect(paddleX, this.refs.canvas.height-paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle =  "#FFEECC";
-    ctx.fill();
-    ctx.closePath();
-
-    if (rightPressed && paddleX < this.refs.canvas.width - paddleWidth) newPaddle = paddleX += 7;
-    if (leftPressed && paddleX > 0) newPaddle = paddleX -= 7;
-    this.setState({
-      paddleX: newPaddle
-    })
-  }
-
-  drawBall = ctx => {
-    ctx.beginPath();
-    ctx.arc(240, 160, 10, 0, Math.PI*2, false);
-    ctx.fillStyle = "#CCCCCC";
-    ctx.fill();
-    ctx.closePath();
-  }
-
+  
   updateCanvas() {
     const ctx = this.refs.canvas.getContext('2d');
     ctx.clearRect(0, 0, 200, 200);
     this.drawPaddle(ctx)
-    this.drawBall(ctx)
   }
-
-  keyDownHandler = (e) => {
-    let rightPressed, leftPressed
-    if (e === 'ArrowRight') rightPressed = true
-    else if (e === 'ArrowLeft') leftPressed = true;
+  
+  handleClick = () => {
     this.setState({
-      rightPressed, 
-      leftPressed
+      game: true
     })
-    this.updateCanvas()
-  }
-
-  keyUpHandler = (e) => {
-    let rightPressed, leftPressed
-    if (e === 'ArrowRight') rightPressed = false
-    else if (e === 'ArrowLeft') leftPressed = false;
-    this.setState({
-      rightPressed, 
-      leftPressed
-    })
-    this.interval = setInterval(() => {
-      this.updateCanvas()
-    })
-  }
-
-  componentDidMount () {
-    this.updateCanvas()
   }
 
 }
