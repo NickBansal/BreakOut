ee4969f2a19b49e1c4f2c1a94646e01bf9e544dc
Nick Bansal <https://github.com/NickBansal>
2018-12-07 09:25:22 +0000

    updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Breakout
+This game was built in Javascript and canvas. This is my first attempt at combining canvas within React
+
+## Step-by-step
+- [x] Create a canvas for my game
+- [x] Create a start button which starts the game    
--- src/BreakOut.js
+++ src/BreakOut.js
@@ -3,26 +3,93 @@ import './App.css';
 
 class App extends Component {
 
-  state = {}
+  state = {
+    paddleHeight: 20,
+    paddleWidth: 80,
+    rightPressed: true, 
+    leftPressed: false,
+    game: true,
+    paddleX: (650-80)/2
+  }
 
   render() {
+    // this.updateCanvas()
     return (
       <div className="App">
         <h1>Breakout</h1>
-      <canvas ref="canvas" width={550} height={350}/>
+        <canvas 
+        tabIndex='0'
+        onKeyDown={(e) => this.keyDownHandler(e.key)}
+        onKeyUp={(e) => this.keyUpHandler(e.key)}
+        ref="canvas" 
+        width={650} 
+        height={350}/>
+        <button>Start Game</button>
       </div>
     );
   }
 
+  drawPaddle = ctx => {
+    ctx.clearRect(0, 0, 200, 200);
+    const { rightPressed, leftPressed, paddleWidth, paddleHeight } = this.state
+    let paddleX = this.state.paddleX
+    let newPaddle
+    ctx.beginPath();
+    ctx.rect(paddleX, this.refs.canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle =  "#FFEECC";
+    ctx.fill();
+    ctx.closePath();
 
-  componentWillMount() {
+    if (rightPressed && paddleX < this.refs.canvas.width - paddleWidth) newPaddle = paddleX += 7;
+    if (leftPressed && paddleX > 0) newPaddle = paddleX -= 7;
     this.setState({
-      canvasSize: {
-        canvasHeight: 350,
-        canvasWidth: 550
-      }
+      paddleX: newPaddle
     })
   }
+
+  drawBall = ctx => {
+    ctx.beginPath();
+    ctx.arc(240, 160, 10, 0, Math.PI*2, false);
+    ctx.fillStyle = "#CCCCCC";
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  updateCanvas() {
+    const ctx = this.refs.canvas.getContext('2d');
+    ctx.clearRect(0, 0, 200, 200);
+    this.drawPaddle(ctx)
+    this.drawBall(ctx)
+  }
+
+  keyDownHandler = (e) => {
+    let rightPressed, leftPressed
+    if (e === 'ArrowRight') rightPressed = true
+    else if (e === 'ArrowLeft') leftPressed = true;
+    this.setState({
+      rightPressed, 
+      leftPressed
+    })
+    this.updateCanvas()
+  }
+
+  keyUpHandler = (e) => {
+    let rightPressed, leftPressed
+    if (e === 'ArrowRight') rightPressed = false
+    else if (e === 'ArrowLeft') leftPressed = false;
+    this.setState({
+      rightPressed, 
+      leftPressed
+    })
+    this.interval = setInterval(() => {
+      this.updateCanvas()
+    })
+  }
+
+  componentDidMount () {
+    this.updateCanvas()
+  }
+
 }
 
 export default App;
